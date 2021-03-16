3403cec72536973fe746a500c0ea5746e6118928
andrewwolf0 <aw.studios@icloud.com>
2021-03-15 22:01:52 -0500

    :briefcase: Clients Index & Contact

---
+++ pages/clients/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Hiya
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="hi there inspect-elementer">
+        <div class="px-8">
+            <div class="w-full mx-auto text-left mt-10">
+                <h1 class="text-4xl text-white font-extrabold">☎️ Want to contact me?</h1>
+                <h2 class="text-3xl text-white font-semibold mt-4">Sounds good, let's <span class="font-extrabold magical-gradient">start chatting</span>.</h2>
+            </div>
+            <MagicalBar class="mb-4" />
+            <div class="w-full mx-auto">
+                <h1 class="text-2xl text-center text-white font-semibold">Ways to Contact</h1>
+                <div class="flex sm:block sm:text-center mt-5">
+                    <a href="https://twitter.com/axiliquint" class="px-5 py-3 border border-transparent text-white font-medium rounded-md mx-2" style="background: #1c95de"><i class="fab fa-twitter"></i> DM me on Twitter</a>
+                    <a href="#" class="px-5 py-3 border border-transparent text-white font-medium rounded-md mx-2" style="background: #7289DA"><i class="fab fa-discord"></i> Talk with me on Discord</a>
+                    <a href="https://twitter.com/axiliquint" class="px-5 py-3 border border-transparent text-white font-medium rounded-md mx-2" style="background: #747474"><i class="fas fa-envelope"></i> Email me</a>
+                </div>
+            </div>
+        </div>
+        <div style="margin-top: 250px">
+        <FooterWave />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
