5ce5e6c224ab7cd23c11bfe96fa77cd4b44a879d
andrewwolf0 <aw.studios@icloud.com>
2021-03-15 22:01:16 -0500

    🧭Navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,35 @@
 <template>
-  <nuxt />
+  <div id="👋">
+    <Navbar />
+    <nuxt />
+  </div>
 </template>
+<script>
+import Navbar from '../components/Navbar.vue'
 
-<style >
+export default {
+  components: {
+    Navbar,
+  }
+}
+</script>
+
+<style>
+.btn-primary {
+  @apply px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600
+}
+.btn-secondary {
+  @apply px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-500 bg-white hover:bg-blue-50
+}
+.magical-gradient {
+    color:transparent;
+    -webkit-background-clip:text;
+    background-clip:text;
+    background-image:linear-gradient(to right,var(--gradient-color-stops));
+    --gradient-from-color: #00D1F7;
+    --gradient-color-stops: var(--gradient-from-color), var(--gradient-to-color, rgba(90, 103, 216, 0));
+    --gradient-via-color: #0091ff;
+    --gradient-color-stops: var(--gradient-from-color), var(--gradient-via-color), var(--gradient-to-color, rgba(213, 63, 140, 0));
+    --gradient-to-color: #438DFC
+}
 </style>
