9e764996541f2caae7e65ef984a3a5cfbef80460
andrewwolf0 <aw.studios@icloud.com>
2021-04-23 23:44:37 -0500

    :wrench: Continued working on Contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,16 +7,28 @@
             </div>
             <MagicalBar class="mb-4" />
             <div class="w-full mx-auto">
-                <h1 class="text-2xl text-center text-white font-semibold">Ways to Contact</h1>
-                <div class="flex sm:block sm:text-center mt-5">
-                    <a href="https://twitter.com/axiliquint" class="px-5 py-3 border border-transparent text-white font-medium rounded-md mx-2" style="background: #1c95de"><i class="fab fa-twitter"></i> DM me on Twitter</a>
-                    <a href="#" class="px-5 py-3 border border-transparent text-white font-medium rounded-md mx-2" style="background: #7289DA"><i class="fab fa-discord"></i> Talk with me on Discord</a>
-                    <a href="https://twitter.com/axiliquint" class="px-5 py-3 border border-transparent text-white font-medium rounded-md mx-2" style="background: #747474"><i class="fas fa-envelope"></i> Email me</a>
+                <h1 class="text-2xl text-center text-white font-semibold mb-4">Pricing</h1>
+                <p class="text-center text-white mb-2">Pricing is highly dependent on the time commitment I will have to put foward on your project.</p>
+                <p class="text-center text-white mb-2">I can either be paid by asset, or hourly. I accept Robux and USD.</p>
+                <p class="text-center text-white">My time is valuable, and I'm sure yours is too. Don't bother requesting me for non-serious projects.</p>
+            </div>
+            <MagicalBar class="mb-4" />
+            <div class="w-full mx-auto">
+                <h1 class="text-2xl text-center text-white font-semibold mb-4">Ways to Contact</h1>
+                <div class="sm:flex items-center justify-center mt-5">
+                    <a href="https://twitter.com/axiliquint" target="_blank" class="px-5 py-3 border border-transparent rounded-md mx-2" style="background: #1c95de">
+                        <p class="text-white font-medium"><i class="fab fa-twitter"></i> DM me on Twitter</p>
+                    </a>
+                    <div class="py-2 sm:py-0"></div>
+                    <a href="#" class="px-5 py-3 border border-transparent rounded-md mx-2 " style="background: #7289DA">
+                        <p target="_blank" class="text-white font-medium"><i class="fab fa-discord"></i> Talk with me on Discord</p>
+                    </a>
+                    <a class="py-2 sm:py-0"></a>
+                    <a href="mailto:andrew@koal.us" class="px-5 py-3 border border-transparent rounded-md mx-2 " style="background: #7F7F7F">
+                        <p class="text-white font-medium" style="background: #747474"><i class="fas fa-envelope"></i> Email me</p>
+                    </a>
                 </div>
             </div>
-        </div>
-        <div style="margin-top: 250px">
-        <FooterWave />
         </div>
     </div>
 </template>
