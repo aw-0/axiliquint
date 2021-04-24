3749fe86362fa0d50d379acdd478a21102fc2a80
andrewwolf0 <aw.studios@icloud.com>
2021-04-23 23:44:22 -0500

    :sparkles: Updated Index

--- pages/index.vue
+++ pages/index.vue
@@ -7,24 +7,27 @@
       </div>
       <MagicalBar class="mb-4" />
       <div class="w-full mx-auto pb-2">
-        <h1 class="text-2xl text-center text-white font-semibold">My Tools</h1>
+        <h1 class="text-2xl text-center text-white font-semibold mb-4">My Tools</h1>
         <div class="md:flex items-center justify-center">
-          <div class="mx-4 mt-4 bg-blue-500 rounded sm:rounded-xl shadow hover:shadow-xl focus:outline-none px-4 py-2">
+          <div class="mx-4 bg-blue-500 rounded sm:rounded-xl shadow hover:shadow-xl focus:outline-none px-4 py-2">
               <a href="https://v3.vuejs.org" target="_blank"><p class="text-center"><i class="fab fa-vuejs text-white my-2 text-8xl"></i></p></a>
               <h1 class="px-2 pt-2 font-semibold text-white text-center m-auto">VueJS</h1>
               <p class="px-2 pt-2 text-white text-center m-auto">VueJS is my web framework of choice</p>
           </div>
-          <div class="mx-4 mt-4 bg-blue-500 rounded sm:rounded-xl shadow hover:shadow-xl focus:outline-none px-4 py-2">
+          <div class="mb-4 sm:mb-0"></div>
+          <div class="mx-4 bg-blue-500 rounded sm:rounded-xl shadow hover:shadow-xl focus:outline-none px-4 py-2">
               <a href="https://python.org" target="_blank"><p class="text-center"><i class="fab fa-python text-white my-2 text-8xl"></i></p></a>
               <h1 class="px-2 pt-2 font-semibold text-white text-center m-auto">Python</h1>
               <p class="px-2 pt-2 text-white text-center m-auto">Fast and easy to write, Python is my favorite language</p>
           </div>
-          <div class="mx-4 mt-4 bg-blue-500 rounded sm:rounded-xl shadow hover:shadow-xl focus:outline-none px-4 py-2">
+          <div class="mb-4 sm:mb-0"></div>
+          <div class="mx-4 bg-blue-500 rounded sm:rounded-xl shadow hover:shadow-xl focus:outline-none px-4 py-2">
               <a href="https://firebase.google.com" target="_blank"><p class="mx-auto"><img src="@/assets/images/firebase.svg" width="250px" height="100px" class="mx-auto object-fill" /></p></a>
               <h1 class="px-2 pt-2 font-semibold text-white text-center m-auto">Firebase</h1>
-              <p class="px-2 pt-2 text-white text-center m-auto">The best PaaS and DBaaS, Firebase</p>
+              <p class="px-2 pt-2 text-white text-center m-auto">The PaaS and DBaaS I'm using <br>for this site, Firebase</p>
           </div>
-          <div class="mx-4 mt-4 bg-blue-500 rounded sm:rounded-xl shadow hover:shadow-xl focus:outline-none px-4 py-2">
+          <div class="mb-4 sm:mb-0"></div>
+          <div class="mx-4 bg-blue-500 rounded sm:rounded-xl shadow hover:shadow-xl focus:outline-none px-4 py-2">
               <a href="https://www.apple.com/final-cut-pro/" target="_blank"><p class="text-center"><i class="fas fa-film text-white my-2 text-8xl"></i></p></a>
               <h1 class="px-2 pt-2 font-semibold text-white text-center m-auto">Final Cut Pro X</h1>
               <p class="px-2 pt-2 text-white text-center m-auto">Robust and powerful, Final Cut Pro X is the video editor I use</p>
@@ -33,7 +36,7 @@
       </div>
       <MagicalBar class="mb-4" />
       <div class="w-full mx-auto">
-        <h1 class="text-2xl text-center text-white font-semibold">Trusted by 1,000,000+</h1>
+        <h1 class="text-2xl text-center text-white font-semibold mb-4">Trusted by 1,000,000+</h1>
         <div class="sm:flex items-center justify-center">
           <a href="https://www.roblox.com/groups/3955051/Foxzies-Productions#!/about" target="_blank" class="sm:mx-auto lg:mx-4"><img src="@/assets/clients/foxzie.png" width="200px" height="150px" class="mt-4 px-4 py-2 mx-auto text-white" alt="Foxzie's Productions"/></a>
           <a href="https://koal.us" target="_blank" class="sm:mx-auto lg:mx-4"><img src="@/assets/clients/koal.svg" width="200px" height="150px" class="svg-white mt-4 px-4 py-2 mx-auto text-white" alt="Koal"/></a>
